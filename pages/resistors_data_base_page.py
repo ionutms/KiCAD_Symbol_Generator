611eabe7d674f122b3b4a68f1fb2cc3a12620cdc
ionutms <mihai.ionut.career@gmail.com>
2024-10-12 19:31:57 +0300

    update app

--- pages/resistors_data_base_page.py
+++ pages/resistors_data_base_page.py
@@ -18,7 +18,7 @@ from dash.dependencies import Input, Output
 import pandas as pd
 
 import pages.utils.style_utils as styles
-# import pages.utils.dash_component_utils as dcu
+import pages.utils.dash_component_utils as dcu
 
 link_name = __name__.rsplit(".", maxsplit=1)[-1].replace("_page", "").title()
 module_name = __name__.rsplit(".", maxsplit=1)[-1]
@@ -96,10 +96,10 @@ layout = dbc.Container([html.Div([
     dbc.Row([dbc.Col([dcc.Link("Go back Home", href="/")])]),
     dbc.Row([dbc.Col([html.H3(
         f"{link_name.replace('_', ' ')}", style=styles.heading_3_style)])]),
-    # dbc.Row([dcu.app_description(TITLE, ABOUT, features, usage_steps)]),
+    dbc.Row([dcu.app_description(TITLE, ABOUT, features, usage_steps)]),
 
     dash_table.DataTable(
-        id='resistor_table',
+        id=f'{module_name}_table',
         columns=create_column_definitions(resistor_dataframe),
         data=resistor_dataframe.to_dict('records'),
         cell_selectable=False,
@@ -114,13 +114,13 @@ layout = dbc.Container([html.Div([
 
 
 @callback(
-    Output("resistor_table", "style_data"),
-    Output("resistor_table", "style_header"),
-    Output("resistor_table", "style_data_conditional"),
-    Output("resistor_table", "style_table"),
-    Output("resistor_table", "style_cell"),
-    Output("resistor_table", "style_filter"),
-    Output("resistor_table", "css"),
+    Output(f'{module_name}_table', "style_data"),
+    Output(f'{module_name}_table', "style_header"),
+    Output(f'{module_name}_table', "style_data_conditional"),
+    Output(f'{module_name}_table', "style_table"),
+    Output(f'{module_name}_table', "style_cell"),
+    Output(f'{module_name}_table', "style_filter"),
+    Output(f'{module_name}_table', "css"),
     Input("theme_switch_value_store", "data"),
 )
 def update_table_style_and_visibility(
