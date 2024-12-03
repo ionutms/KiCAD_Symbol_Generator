37417831f00219cfdc9adcbd73785825113a1aa0
ionutms <mihai.ionut.career@gmail.com>
2024-12-03 15:09:04 +0200

    add more n-ch transisitors

--- scripts/transistors/symbol_transistor_specs.py
+++ scripts/transistors/symbol_transistor_specs.py
@@ -47,40 +47,71 @@ class PartInfo(NamedTuple):
 
 SERIES_SPECS: dict[str, SeriesSpec] = {
     "SI7309DN-T1-GE3": SeriesSpec(
-        manufacturer="Vishay Semiconductors",
-        base_series="SI7309DN-T1-GE3",
+        manufacturer="Vishay Semiconductors", base_series="SI7309DN-T1-GE3",
         footprint="transistor_footprints:PowerPAK 1212-8",
         datasheet="https://www.vishay.com/docs/73434/si7309dn.pdf",
-        drain_source_voltage =[-60.0],
-        drain_current=[-8.0],
-        package="PowerPAK 1212-8",
-        transistor_type="P-Channel",
-        trustedparts_link="https://www.trustedparts.com/en/search",
-    ),
-    "PSMN075-100MSEX": SeriesSpec(
-        manufacturer="Nexperia",
-        base_series="PSMN075-100MSEX",
-        footprint="transistor_footprints:LFPAK-33-8",
-        datasheet=(
-            "https://assets.nexperia.com/documents/data-sheet/"
-            "PSMN075-100MSE.pdf"),
-        drain_source_voltage =[100.0],
-        drain_current=[18],
-        package="LFPAK-33-8",
-        transistor_type="N-Channel",
-        trustedparts_link="https://www.trustedparts.com/en/search",
-    ),
+        drain_source_voltage =[-60.0], drain_current=[-8.0],
+        package="PowerPAK 1212-8", transistor_type="P-Channel",
+        trustedparts_link="https://www.trustedparts.com/en/search"),
+
     "PSMN040-100MSEX": SeriesSpec(
-        manufacturer="Nexperia",
-        base_series="PSMN040-100MSEX",
+        manufacturer="Nexperia", base_series="PSMN040-100MSEX",
         footprint="transistor_footprints:LFPAK-33-8",
         datasheet=(
             "https://assets.nexperia.com/documents/data-sheet/"
             "PSMN040-100MSE.pdf"),
-        drain_source_voltage =[100.0],
-        drain_current=[30],
-        package="LFPAK-33-8",
-        transistor_type="N-Channel",
+        drain_source_voltage =[100.0], drain_current=[30],
+        package="LFPAK-33-8", transistor_type="N-Channel",
+        trustedparts_link="https://www.trustedparts.com/en/search"),
+
+    "BUK9M34-100EX": SeriesSpec(
+        manufacturer="Nexperia", base_series="BUK9M34-100EX",
+        footprint="transistor_footprints:LFPAK-33-8",
+        datasheet=(
+            "https://assets.nexperia.com/documents/data-sheet/"
+            "BUK9M34-100E.pdf"),
+        drain_source_voltage =[100.0], drain_current=[29],
+        package="LFPAK-33-8", transistor_type="N-Channel",
+        trustedparts_link="https://www.trustedparts.com/en/search"),
+
+    "BUK9M43-100EX": SeriesSpec(
+        manufacturer="Nexperia", base_series="BUK9M43-100EX",
+        footprint="transistor_footprints:LFPAK-33-8",
+        datasheet=(
+            "https://assets.nexperia.com/documents/data-sheet/"
+            "BUK9M43-100E.pdf"),
+        drain_source_voltage =[100.0], drain_current=[25],
+        package="LFPAK-33-8", transistor_type="N-Channel",
+        trustedparts_link="https://www.trustedparts.com/en/search"),
+
+    "PSMN075-100MSEX": SeriesSpec(
+        manufacturer="Nexperia", base_series="PSMN075-100MSEX",
+        footprint="transistor_footprints:LFPAK-33-8",
+        datasheet=(
+            "https://assets.nexperia.com/documents/data-sheet/"
+            "PSMN075-100MSE.pdf"),
+        drain_source_voltage =[100.0], drain_current=[18],
+        package="LFPAK-33-8", transistor_type="N-Channel",
+        trustedparts_link="https://www.trustedparts.com/en/search",
+    ),
+    "BUK9M120-100EX": SeriesSpec(
+        manufacturer="Nexperia", base_series="BUK9M120-100EX",
+        footprint="transistor_footprints:LFPAK-33-8",
+        datasheet=(
+            "https://assets.nexperia.com/documents/data-sheet/"
+            "BUK9M120-100E.pdf"),
+        drain_source_voltage =[100.0], drain_current=[11.5],
+        package="LFPAK-33-8", transistor_type="N-Channel",
+        trustedparts_link="https://www.trustedparts.com/en/search",
+    ),
+    "BUK9M156-100EX": SeriesSpec(
+        manufacturer="Nexperia", base_series="BUK9M156-100EX",
+        footprint="transistor_footprints:LFPAK-33-8",
+        datasheet=(
+            "https://assets.nexperia.com/documents/data-sheet/"
+            "BUK9M156-100E.pdf"),
+        drain_source_voltage =[100.0], drain_current=[9.3],
+        package="LFPAK-33-8", transistor_type="N-Channel",
         trustedparts_link="https://www.trustedparts.com/en/search",
     ),
 }
