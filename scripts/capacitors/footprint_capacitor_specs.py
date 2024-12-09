73089b1858d5c20c7c16c76ad9d80160c74d5fae
ionutms <mihai.ionut.career@gmail.com>
2024-12-09 19:47:47 +0200

    small capacitor footprint adjustments

--- scripts/capacitors/footprint_capacitor_specs.py
+++ scripts/capacitors/footprint_capacitor_specs.py
@@ -38,19 +38,19 @@ class FootprintSpecs(NamedTuple):
 
 FOOTPRINTS_SPECS: dict[str, FootprintSpecs] = {
     "0402": FootprintSpecs(
-        body_dimensions=BodyDimensions(width=1.82, height=0.955),
-        pad_dimensions=PadDimensions(width=0.6, height=0.7, center_x=0.54),
+        body_dimensions=BodyDimensions(width=1.82, height=0.92),
+        pad_dimensions=PadDimensions(width=0.56, height=0.62, center_x=0.48),
         ref_offset_y=-1.27),
     "0603": FootprintSpecs(
-        body_dimensions=BodyDimensions(width=2.96, height=1.54),
-        pad_dimensions=PadDimensions(width=0.9, height=1.0, center_x=0.875),
+        body_dimensions=BodyDimensions(width=2.96, height=1.46),
+        pad_dimensions=PadDimensions(width=0.9, height=0.95, center_x=0.775),
         ref_offset_y=-1.524),
     "0805": FootprintSpecs(
-        body_dimensions=BodyDimensions(width=3.36, height=2.09),
-        pad_dimensions=PadDimensions(width=1.15, height=1.45, center_x=0.95),
+        body_dimensions=BodyDimensions(width=3.4, height=1.96),
+        pad_dimensions=PadDimensions(width=1.0, height=1.45, center_x=0.95),
         ref_offset_y=-1.778),
     "1206": FootprintSpecs(
-        body_dimensions=BodyDimensions(width=4.56, height=2.74),
-        pad_dimensions=PadDimensions(width=1.25, height=1.8, center_x=1.5),
+        body_dimensions=BodyDimensions(width=4.6, height=2.3),
+        pad_dimensions=PadDimensions(width=1.15, height=1.8, center_x=1.475),
         ref_offset_y=-2.032),
 }
